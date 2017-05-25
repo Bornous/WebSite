7d9b13fe1b5ff69ce1a192a9f3b4f101efce51c2
Bornous <pavelgra77@gmail.com>
2017-05-25 02:57:58 +0200

    A lot of things
    -adding gallery
    -improving some details like icons
    - unification of background style in all sites

--- myjavascript.js
+++ myjavascript.js
@@ -3,7 +3,7 @@
 	var aDayName= presentTime.getDay();
 	var month = presentTime.getMonth();
 	var year = presentTime.getFullYear();
-	console.log(aDayDate);
+	
 	var days = new Array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
 	var months= new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
 	var presentMonth= months[month];
@@ -34,7 +34,6 @@
 		document.getElementById("destinationTown").innerHTML="<h1> "+localStorage.destination+" </h1>";
 
 		var day1= presentTime.getDate();
-		console.log(day1);
 		var month1=presentTime.getMonth()+1;
 		var year1=year;
 		//TESTS:
@@ -64,7 +63,7 @@
 					month1++;
 			}
 			else if(day1==29 && month1==2){
-						console.log(year1%4);
+						
 					day1=0;
 					month1++;
 			}
@@ -94,6 +93,12 @@
 	}
 	
 	
+	function submit(){
+		console.log("spin-yes");
+		document.getElementById("progres").innerHTML=' <p><i class="fa fa-spinner w3-spin" style="font-size:64px"></i></p> ';
+	}
+	
+	
 	function linkToHomePage() {
 		window.location.href="index.html";
 		
@@ -107,8 +112,66 @@
 	
 	function linkToForm(city){
 		localStorage.destination = city;
-		console.log(city);
+		
 		window.location.href="Form.html";
 		
 	}
 	
+	//Pokaz slajdów
+		 
+		var slideIndex = 1;
+		var whichCity;
+	
+	$(document).ready(function(){
+		
+		$("#galleryRioDe").hide();
+		$("#galleryMonte").hide();
+		$("#galleryVerna").hide();
+		
+		$("#galleryRio").click(function(){
+				$("#choosingDiv").hide();
+					$("#galleryRioDe").show();
+					$("#galleryMonte").hide();
+					$("#galleryVerna").hide();
+					whichCity=0;
+					plusDivs(slideIndex);
+			});
+			
+		$("#galleryMon").click(function(){
+				$("#choosingDiv").hide();
+					$("#galleryMonte").show();
+					$("#galleryRioDe").hide();
+					$("#galleryVerna").hide();
+					whichCity=1;
+					plusDivs(slideIndex);
+			});
+			
+		$("#galleryVer").click(function(){
+				$("#choosingDiv").hide();
+					$("#galleryVerna").show();
+					$("#galleryRioDe").hide();
+					$("#galleryMonte").hide();
+				
+					whichCity=2;
+					plusDivs(slideIndex);
+			});
+	
+
+	});
+	
+	function plusDivs(n) {
+		  showDivs(slideIndex += n);
+		}
+
+		function showDivs(n) {
+		  var i;
+		  var x = document.getElementsByClassName("mySlides"+whichCity);
+		  if (n > x.length) {slideIndex = 1}    
+		  if (n < 1) {slideIndex = x.length}
+		  console.log("SlideIndex="+slideIndex);
+		  for (i = 0; i < x.length; i++) {
+			 x[i].style.display = "none";  
+		  }
+		  
+		 if(x[slideIndex-1])x[slideIndex-1].style.display = "block";  
+		}
